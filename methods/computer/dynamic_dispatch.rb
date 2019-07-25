65357ccf7faf29a22eae2b06f05eefc638442e65
vviilloovv <vviilloovv@gmail.com>
2019-07-25 15:41:11 +0900

    リファクタリング

--- methods/computer/dynamic_dispatch.rb
+++ methods/computer/dynamic_dispatch.rb
@@ -4,23 +4,17 @@ class Computer
     @data_source = data_source
   end
 
-  def mouse
-    component :mouse
+  def self.define_component(name)
+    define_method(name) do
+      info = @data_source.send "get_#{name}_info", @id
+      price = @data_source.send "get_#{name}_price", @id
+      result = "#{name.capitalize}: #{info} ($#{price})"
+      return "* #{result}" if price >= 100
+      result
+    end
   end
 
-  def cpu
-    component :mouse
-  end
-
-  def keyboard
-    component :keyboard
-  end
-
-  def component(name)
-    info = @data_source.send "get_#{name}_info", @id
-    price = @data_source.send "get_#{name}_price", @id
-    result = "#{name.capitalize}: #{info} ($#{price})"
-    return "* #{result}" if price >= 100
-    result
-  end
+  define_component :mouse
+  define_component :cpu
+  define_component :keyboard
 end

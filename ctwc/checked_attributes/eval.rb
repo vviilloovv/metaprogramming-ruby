8704b6072198666f55d9507d4b1500a428053871
vviilloovv <vviilloovv@gmail.com>
2019-08-14 10:59:34 +0900

    evalを使って実装

--- ctwc/checked_attributes/eval.rb
+++ ctwc/checked_attributes/eval.rb
@@ -27,4 +27,16 @@ class TestCheckedAttribute < Minitest::Test
 end
 
 def add_checked_attribute(klass, attribute)
+  eval <<~EOS
+    class #{klass}
+      def #{attribute}=(value)
+        raise "Invalid attribute" unless value
+        @#{attribute} = value
+      end
+
+      def #{attribute}()
+        @#{attribute}
+      end
+    end
+  EOS
 end
